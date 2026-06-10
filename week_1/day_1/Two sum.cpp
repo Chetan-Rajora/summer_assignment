3299c731162c1014890289c8cc3e65f2b837597a
Chetan-Rajora <rajorachetan008@gmail.com>
2026-06-10 09:44:16 +0530

    Create Two sum.cpp

---
+++ week_1/day_1/Two sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for(int i =0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==j)
+                {
+                    continue;
+                }
+                else
+                {
+                    if(target ==(nums[i]+nums[j]))
+                    {
+                       return {i,j};
+                    }
+                }
+            }
+        }
+        return {};
+    }
+};
